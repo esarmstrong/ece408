d511439e6716d4333de0ec3447d876b502894997
Stephen Hurwit <10472810+shurwit@users.noreply.github.com>
2018-11-30 22:39:00 -0600

    Working on unrolling...

--- new-forward.cuh
+++ new-forward.cuh
@@ -5,6 +5,8 @@
 #include <mxnet/base.h>
 
 #define TILE_WIDTH 16
+#define CUDA_MAX_NUM_THREADS 1024
+
 __constant__ float weightMatrix[24*12*7*7];
 
 namespace mxnet
@@ -95,7 +97,7 @@ __global__ void forward_kernel_constant(float *y, const float *x, const float *k
 	#undef y4d
 	#undef x4d
 	#undef k4d
-  #undef ck4d
+ 	#undef ck4d
 }
 
 __global__ void forward_kernel_shared(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
@@ -112,12 +114,12 @@ __global__ void forward_kernel_shared(float *y, const float *x, const float *k, 
 	#define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
 	#define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
-	int n, m, h0, w0, h_base, w_base, h, w;
+	int b, m, h0, w0, h_base, w_base, h, w;
 	int X_tile_width = TILE_WIDTH + K-1;
 	extern __shared__ float shared_mem[];
 	float* X_shared = &shared_mem[0];
 	float* W_shared = &shared_mem[X_tile_width * X_tile_width];
-	n = blockIdx.x;
+	b = blockIdx.x;
 	m = blockIdx.y;
 	h0 = threadIdx.y;
 	w0 = threadIdx.x;
@@ -139,7 +141,7 @@ __global__ void forward_kernel_shared(float *y, const float *x, const float *k, 
 		for (int i = h; i < h_base + X_tile_width; i += TILE_WIDTH) {
 			for (int j = w; j < w_base + X_tile_width; j += TILE_WIDTH) {
 				if (i < H && j < W) {
-					X_shared[(i - h_base) * X_tile_width + (j - w_base)] = x4d(n, c, i, j);
+					X_shared[(i - h_base) * X_tile_width + (j - w_base)] = x4d(b, c, i, j);
 				} else {
 					X_shared[(i - h_base) * X_tile_width + (j - w_base)] = 0;
 				}
@@ -157,61 +159,41 @@ __global__ void forward_kernel_shared(float *y, const float *x, const float *k, 
 		__syncthreads();
 	}
 
-	if (n < B && m < M && h < H_out && w < W_out) {
-		y4d(n, m, h, w) = acc;
+	if (b < B && m < M && h < H_out && w < W_out) {
+		y4d(b, m, h, w) = acc;
 	}
 	#undef y4d
 	#undef x4d
 	#undef k4d
 }
 
-__global__ void unroll_Kernel(int C, int H, int W, int n, int K, float* x, float* X_unroll)
+__global__ void unroll_Kernel(int C, int H, int W, int b, int K, float* x, float* X_unroll)
 {
 	#define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
-	int c, s, h_out, w-out, h_unroll, w_base, p, q;
-	int t = blockIdx.x * 1024 + threadIdx.x;
-	int H_out = H – K + 1;
-	int W_out = W – K + 1;
+	int c, s, row_out, col_out, col_unroll, row_unroll, w_base, p, q;
+	int t = blockIdx.x * CUDA_MAX_NUM_THREADS + threadIdx.x;
+	int H_out = H - K + 1;
+	int W_out = W - K + 1;
 	int W_unroll = H_out * W_out;
+	int H_unroll = C * K * K;
 	if (t < C * W_unroll) {
 		c = t / W_unroll;
 		s = t % W_unroll;
-		h_out = s / W_out;
-		w_out = s % W_out;
-		h_unroll = h_out * W_out + w_out;
+		row_out = s / W_out;
+		col_out = s % W_out;
+		row_unroll = row_out * W_out + col_out;
 		w_base = c * K * K;
 		for(p = 0; p < K; p++) {
 			for(q = 0; q < K; q++) {
-				w_unroll = w_base + p * K + q;
-				X_unroll(h_unroll*W_unroll + w_unroll) = x4d(n, c, h_out + p, w_out + q);
+				if (row_out + p < H && col_out + q < W && row_unroll < H_unroll && col_unroll < W_unroll) {
+					col_unroll = w_base + p * K + q;
+					X_unroll[row_unroll * W_unroll + col_unroll] = x4d(b, c, row_out + p, col_out + q);
+				}
 			}
 		}
 	}
 	#undef x4d
 }
-
-void unroll_gpu(int C, int H, int W, int n, int K, float* X, float* X_unroll)
-{
-	int H_out = H – K + 1;
-	int W_out = W – K + 1;
-	int num_threads = C * H_out * W_out;
-	int num_blocks = ceil((C * H_out * W_out) / 1024);
-	unroll_Kernel<<<num_blocks, 1024>>>(C, H, W, n, K, X, X_unroll);
-}
-
-void convLayer_forward(int N, int M, int C, int H, int W, int K, float* X, float* W_unroll, float* Y)
-{
-	int W_out = W – K + 1;
-	int H_out = H – K + 1;
-	int W_unroll = C * K * K;
-	int H_unroll = H_out * W_out;
-	float* X_unrolled = malloc(W_unroll * H_unroll * sizeof(float));
-	for (int n=0; n < N; n++) {
-		unroll_gpu(C, H, W, K, n, X, X_unrolled);
-		gemm(H_unroll, M, W_unroll, X_unrolled, W, Y[n]);
-	}
-}
-
 
 __global__ void matrixMultiplyShared(float *A, float *B, float *C,  
                                      int numARows, int numAColumns,
@@ -301,18 +283,34 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     forward_kernel_shared<<<gridDim, blockDim, shared_size>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
     */
 	
+	
+	int W_unroll = H_out * W_out;
+	int H_unroll = C * K * K;
+	
+	float* X_unrolled;
+	cudaMalloc((void **) &X_unrolled, W_unroll * H_unroll * sizeof(float));
+
+	dim3 dimBlock(TILE_WIDTH, TILE_WIDTH, 1); 
+	dim3 dimGrid(ceil((1.0*H_unroll)/TILE_WIDTH), ceil((1.0*M)/TILE_WIDTH), 1);
+
+	int num_blocks = ceil((C * H_out * W_out) / CUDA_MAX_NUM_THREADS);
+
+	for (int b = 0; b < B; b++) {
+		float* curr_output = &y.dptr_[b * M * H_out * W_out];
+		unroll_Kernel<<<num_blocks, CUDA_MAX_NUM_THREADS>>>(C, H, W, b, K, x.dptr_, X_unrolled);
+
+		matrixMultiplyShared<<<dimGrid, dimBlock>>>(w.dptr_, X_unrolled, curr_output, 
+													M, H_unroll, 
+													H_unroll, W_unroll, 
+													M, W_unroll);
+	}
+	
 	/*
 	//Matrix Multiply call
-	dim3 dimGrid(ceil((1.0*numCColumns)/TILE_WIDTH), ceil((1.0*numCRows)/TILE_WIDTH), 1);
-  
-	dim3 dimBlock(TILE_WIDTH, TILE_WIDTH, 1); 
 	  
 	wbTime_start(Compute, "Performing CUDA computation");
 	//@@ Launch the GPU Kernel here
-	matrixMultiplyShared<<<dimGrid, dimBlock>>>(deviceA, deviceB, deviceC, 
-												numARows, numAColumns, 
-												numBRows, numBColumns, 
-												numCRows, numCColumns);
+	
 	cudaDeviceSynchronize();
 	*/
 
